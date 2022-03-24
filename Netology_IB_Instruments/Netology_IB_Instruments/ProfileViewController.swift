6c11406f2afc5fb28474ab56f3fca5ab0c782745
Myakkiev <stanislavmyakkiev@gmail.com>
2022-03-24 22:39:42 +0300

    Home work

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by 1 on 24.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthLabel: UILabel!
+     
+    @IBOutlet weak var cityLabel: UILabel!
+  
+    
+    @IBOutlet weak var avaImage: UIImageView!
+    
+    
+    @IBOutlet weak var myText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
